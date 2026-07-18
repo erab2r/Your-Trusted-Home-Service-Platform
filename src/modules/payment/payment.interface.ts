1e74a5f5838f3323347af723aec28d354c24a134
erab2r <shkera213@gmail.com>
2026-07-18 21:24:36 +0600

    paymentinterface update

--- src/modules/payment/payment.interface.ts
+++ src/modules/payment/payment.interface.ts
@@ -1,12 +1,11 @@
 import { PaymentStatus } from "../../../generated/prisma/enums";
 
-export interface ICreateCheckoutSession {
-  bookingId: string;
-}
-
 export interface IStripeMetadata {
   bookingId: string;
   paymentId: string;
+}
+export interface ICreateCheckoutSession {
+  bookingId: string;
 }
 
 export interface IPaymentQuery {
